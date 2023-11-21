c114a0bed94f39a933ee58a907ce551c4e40f248
Ezekwe chibuikem <nwachinaemelum@yahoo.com>
2023-11-21 10:42:26 +0100

    worked on the index view

--- HrManagement/HrManagement/settings.py
+++ HrManagement/HrManagement/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leave',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'HrManagement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Layouts')],
+        'DIRS': [os.path.join(BASE_DIR, 'HrManagement/leave/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'leave/static/assets')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
